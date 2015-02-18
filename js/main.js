00aec9762c3b296dbd78373f357a46d78823be55
Garrett Graham <ganderson7197@icloud.com>
2015-02-18 16:50:09 -0500

    coment out

--- js/main.js
+++ js/main.js
@@ -9,7 +9,11 @@
 
 //use below as an array to be looped through and populate the html with quote data 
 
+//
 //var _ = require('lodash')
+//
+//$(document).ready(function() {
+
 var firstLady = 
 [
     // index: 0
@@ -186,10 +190,51 @@ var firstLady =
 
 var counter = 0
 
-for (var counter = 0; counter < 10; counter++){
-    console.log(firstLady[counter].contents.quote)
-}
+$('#target').click(function() {
+    $('#output').html(firstLady[counter].contents.quote)
+        return counter++
+    });
+console.log(counter);
 
+
+//
+//$('#target').click(function() {
+//    $('#output').html(firstLady[counter].contents.quote)
+//    return counter++
+//});
+//
+//console.log(counter);
+
+//});
+//
+//if(counter > 10) {
+//    return counter = counter[0]
+//}
+//else {
+//    return counter = counter++
+//}
+
+//    if (counter is too big){
+//    return counter to 0
+//}
+//else {
+//    return counter and add 1 
+//}
+//    return counter++
+//    
+//});
+
+//if (var counter = 0; counter < 10; counter++){
+//    console.log(firstLady[counter].contents.quote)
+//} 
+
+
+
+
+//
+//var counter = 0
+//
+//for 
 //console.log(counter)
 
 
@@ -199,5 +244,5 @@ for (var counter = 0; counter < 10; counter++){
 //function quote(){ 
 //    alert ("Check out this quote from" + firstLady[0].contents.author + " " + firstLady[1].contents.quote)
 //}
-//
+////
 
