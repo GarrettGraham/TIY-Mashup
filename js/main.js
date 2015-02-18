f3e60e349111711450773c4873a2c4706247c50b
Garrett Graham <ganderson7197@icloud.com>
2015-02-18 14:30:09 -0500

    logan added button tag to index.html to demonstrate a function for an alert and i created an array of the json data within the main.js file so i dont need to worry about source coding when keeping the array in a .json file. commented out the function aler from logan and now need to merge the feature/02 branch into master and create the third branch for the loops

--- js/main.js
+++ js/main.js
@@ -1,7 +1,181 @@
 //use below as an array to be looped through and populate the html with quote data 
 
-var quotesER = require('../TIY-Mashup/EleanorRoosevelt.json')
+//var firstLady = require('../TIY-Mashup/EleanorRoosevelt.json')
 
-var quote = quotesER.filter(function(item) {
-   return item.quote = 'Quote' 
-});
+//var quote = contents.filter(function(item) {
+//   return item.quote = 'Quote' 
+//});
+
+
+//use below as an array to be looped through and populate the html with quote data 
+
+var firstLady = 
+[
+    // index: 0
+{
+"success": {
+"total": 1
+},
+"contents": {
+"quote": "People grow through experience if they meet life honestly and courageously. This is how character is built.",
+"author": "Eleanor Roosevelt",
+"id": "oPmrJij8JxaKuZttCxNBaAeF",
+"requested_category": null,
+"categories": [
+"character",
+"experience",
+"life",
+"people"
+]
+}
+},
+   // index: 1
+{
+"success": {
+"total": 1
+},
+"contents": {
+"quote": "I believe that anyone can conquer fear by doing the things he fears to do, provided he keeps doing them until he gets a record of successful experience behind him.",
+"author": "Eleanor Roosevelt",
+"id": "Azh8yQeLeTivSWlrJbbC6AeF",
+"requested_category": null,
+"categories": [
+"experience"
+]
+}
+},
+    // index: 2
+{
+"success": {
+"total": 1
+},
+"contents": {
+"quote": "Don't call a woman a bitch. Call her an ass-hole. It still gets your point across and it's not sexist.",
+"author": "Eleanor Roosevelt",
+"id": "Zc_jFz69rqwNgaBsx5zBfgeF",
+"requested_category": null,
+"categories": [
+"funny",
+"humor",
+"sexism",
+"vocabulary"
+]
+}
+},
+    // index: 3
+{
+"success": {
+"total": 1
+},
+"contents": {
+"quote": "What could we accomplish if we knew we could not fail?",
+"author": "Eleanor Roosevelt",
+"id": "2s4HIn1xsfvDqpb6zpyHkweF",
+"requested_category": null,
+"categories": [
+"confidence",
+"timidity"
+]
+}
+},
+    // index: 4
+{
+"success": {
+"total": 1
+},
+"contents": {
+"quote": "I think if the people of this country can be reached with the truth, their judgment will be in favor of the many, as against the privileged few",
+"author": "Eleanor Roosevelt",
+"id": "GZcU4A5YTNFVc6Cna19JGweF",
+"requested_category": null,
+"categories": [
+"citizenship",
+"judgement",
+"truth"
+]
+}
+},
+    // index: 5
+{
+"success": {
+"total": 1
+},
+"contents": {
+"quote": "No one can make you feel inferior without your consent.",
+"author": "Eleanor Roosevelt",
+"id": "MF3ZuyMj4HWW3ydaifWNDgeF",
+"requested_category": null,
+"categories": [
+"inferiority"
+]
+}
+},
+    // index: 6
+{
+"success": {
+"total": 1
+},
+"contents": {
+"quote": "Perhaps nature is our best assurance of immortality.",
+"author": "Eleanor Roosevelt",
+"id": "dMyt_SXzSz98CxXlKsgcDgeF",
+"requested_category": null,
+"categories": [
+"nature"
+]
+}
+},
+    // index: 7
+{
+"success": {
+"total": 1
+},
+"contents": {
+"quote": "It takes courage to love, but pain through love is the purifying fire which those who love generously know. We all know people who are so much afraid of pain that they shut themselves up like clams in a shell and, giving out nothing, receive nothing and therefore shrink until life is a mere living death.",
+"author": "Eleanor Roosevelt",
+"id": "X7lrfVv0RhEXX_9xGVRZGQeF",
+"requested_category": null,
+"categories": [
+"love"
+]
+}
+},
+    // index: 8
+{
+"success": {
+"total": 1
+},
+"contents": {
+"quote": "Do one thing everyday that scares you.",
+"author": "Eleanor Roosevelt",
+"id": "CLLdacjYfmMxSRKHLv_ulQeF",
+"requested_category": null,
+"categories": [
+"change",
+"courage",
+"growth"
+]
+}
+},
+    // index: 9
+{
+"success": {
+"total": 1
+},
+"contents": {
+"quote": "People grow through experience if they meet life honestly and courageously. This is how character is built.",
+"author": "Eleanor Roosevelt",
+"id": "OU4aMVEF1C6vwt3WmN7pVweF",
+"requested_category": null,
+"categories": [
+"character"
+]
+}
+}
+
+]
+
+function quote(){ 
+    alert ("Check out this quote from" + firstLady[0].contents.author + " " + firstLady[1].contents.quote)
+}
+    
