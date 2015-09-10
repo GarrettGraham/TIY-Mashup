0b325af5a21422763373c804151f5cb9b08c2eae
Garrett Graham <ganderson7197@icloud.com>
2015-09-10 12:50:14 -0400

    bower installed angular flexslider plugin and wiredepped the source code into the index. added the flexslider to the angular dependencies

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 (function(){
 var app = angular.module('EleanorQuotes', []);
 
-app.controller('QuotesCtrl', ['$scope', '$http',
-  function($scope, $http) {
+app.controller('QuotesCtrl', ['$scope', '$http', 'angular-flexslider'
+  function($scope, $http, angular-flexslider) {
     $http.get('js/quotes.json').success(function(data) {
       $scope.quotes = []; //get data from json
       angular.forEach(data, function(quote) {
