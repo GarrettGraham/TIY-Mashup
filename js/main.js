783001cb4f84f2b961f59105d381aa66cb3ebc22
Garrett Graham <ganderson7197@icloud.com>
2015-09-11 12:46:18 -0400

    moved the container for the quote slider with css and would like to add opacity to it. removed the closing tag from the html item but replaced it. been playing with jquery in order to get the background images to fade into one another instead of jumping from one to the next as it does with the settimeout method. been bulldogging but want the images to transition smoothly

--- js/main.js
+++ js/main.js
@@ -7,14 +7,15 @@ $(function(){
     'url(https://res.cloudinary.com/theyogime/image/upload/v1440793409/IMG_2253_pqbov8.jpg)',
     'url(https://res.cloudinary.com/theyogime/image/upload/v1440791164/Savannah_Sunset_q8l4l2.jpg)',
     'url(https://res.cloudinary.com/theyogime/image/upload/v1440791206/IMG_3493_ye8ppt.jpg)',
-    'url(https://res.cloudinary.com/theyogime/image/upload/v1440793134/IMG_2400_vrn8u5.jpg)'
+    'url(https://res.cloudinary.com/theyogime/image/upload/v1440793134/IMG_2400_vrn8u5.jpg)',
+    'url(https://res.cloudinary.com/theyogime/image/upload/v1440791005/IMG_2232_anlbdp.jpg)'
   ];
   var current = 0;
 
   function nextBackground() {
     body.css(
       'background-image',
-      backgrounds[current = ++current % backgrounds.length]
+       backgrounds[current = ++current % backgrounds.length]
     );
     setTimeout(nextBackground, 5000);
   }
